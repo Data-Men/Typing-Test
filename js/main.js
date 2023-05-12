40bc0127150c612e0c04d865562598d880dbb23b
Data-Men <nee271raj@protonmail.com>
2023-05-12 11:38:08 +0530

    timer and file arranged

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-
 let count = 0;
-let time=0;
+// const reader=new FileReader();
+// reader.onload=()=>{
+//   const text=reader.result;
+// }
+// console.log( reader.readAsText("./../public/paragraph1.txt"));
 window.onload = () => {
   const inputarea = document.getElementById("inputtext");
   const readText = document.getElementById("readArea");
-
-  // readText.innerText=
+  inputarea.focus();
+  
   inputarea.addEventListener("keyup", (event) => {
-    const keySound = new Audio("keyboard.mp3");
+    const keySound = new Audio("/./../public/keyboard.mp3");
+    keySound.playbackRate = 1.1;
     keySound.play();
     if (event.key != "Enter") {
       const readArea = document.getElementById("readArea");
@@ -31,38 +35,61 @@ window.onload = () => {
             }</span>`;
         }
       }
-      text =
-        text +
+      text +=
         readArea.innerText
           .toString()
           .substring(index, readArea.innerText.length);
       readArea.innerHTML = text;
 
-      if (count==readArea.innerText.length) {
+      if (count == readArea.innerText.length) {
         console.log(Date.now());
-      }else if (count==0 && !['Ctrl','Shift','Alt'].includes(event.key)) {
-        // time=Date.now();
-
+      } else if (count == 0 && !["Ctrl", "Shift", "Alt"].includes(event.key)) {
         timer();
       }
 
       count++;
-
     }
   });
 };
 
-
 //Timer
-function  timer() {
-  const timer= document.getElementById("timer");
-  const x=setInterval(()=>{
-let text=timer.innerText;
-console.log(text);
-timer.innerText=Number(text)-1
-if (Number(text)-1< 0) {
-  clearInterval(x);
-  document.getElementById("timer").innerHTML = "EXPIRED";
-}
-   },1000)
+function timer() {
+  const timer = document.getElementById("timer");
+  const x = setInterval(() => {
+    let str = "Time ";
+    console.log(timer.innerText);
+    const text = timer.innerText.substring(5, 10);
+    const min = Number(text.substring(0, text.indexOf(":")));
+    const second = Number(text.substring(text.indexOf(":") + 1, 5));
+    if (min > 0) {
+      if (second == 0) {
+        str +=
+          ((min - 1).toString().length != 2 ? "0" : "") +
+          (min - 1).toString() +
+          ":";
+        str += "59";
+      } else if (second > 0) {
+        str = str + (min.toString().length != 2 ? "0" : "") + min + ":";
+        str +=
+          ((second - 1).toString().length != 2 ? "0" : "") +
+          (second - 1).toString();
+      }
+    } else {
+      str +=
+        "00:" +
+        ((second - 1).toString().length != 2 ? "0" : "") +
+        (second - 1).toString();
+    }
+    timer.innerText = str;
+    if (second <= 10 && min == 0) timer.style.color = "Red";
+
+    if (second == 1 && min == 0) {
+      clearInterval(x);
+      const TaskComplete = new Audio("./../public/correct-choice.mp3");
+      TaskComplete.play();
+      document.getElementById("statsWindow").style.display = "block";
+      document.getElementById("Restart").focus();
+      document.getElementById("overlay").style.display = "block";
+    }
+  }, 1000);
 }
